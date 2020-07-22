a18696a2aac84836f0e4c334c6a46f0e10482b41
Susan Strickler <srs218@users.noreply.github.com>
2020-07-22 16:46:02 -0400

    Update hisat_se_annot.sh

--- annotation/hisat_se_annot.sh
+++ annotation/hisat_se_annot.sh
@@ -25,7 +25,7 @@ for file in `dir -d *.fastq.gz` ; do
 done
 
 ls *.sam |parallel --gnu -j $CPU samtools view -Sb -o {.}.bam {}
-ls *.sort.bam |parallel --gnu -j $CPU samtools flagstat {} ">" {.}.flagstat
+ls *.bam |parallel --gnu -j $CPU samtools flagstat {} ">" {.}.flagstat
 cat *flagstat |grep "mapped (" |sed 's/.*(\(.*\)%.*/\1/g' |awk '{sum+=$1} END { print "Average = ",sum/NR}' > average_mapping.txt
 
 #calc mapping percent
@@ -36,7 +36,7 @@ for file in `dir -d *.flagstat` ; do
 
 done
 
-for file in `ls -1 *mapped`; do
+for file in `ls -1 *mapped` ; do
     echo "$file" > ./tmpfile
     cat "$file" >> ./tmpfile
     mv ./tmpfile "$file"
@@ -46,7 +46,7 @@ done
 cat *.csv > mapping_efficiency.csv
 
 #run stringtie
-for file in `dir -d *.sort.bam` ; do
+for file in `dir -d *.bam` ; do
     
     outdir=`echo "$file" |sed 's/.bam//'`
     
