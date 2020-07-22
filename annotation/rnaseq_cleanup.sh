b59302073639f3caecf3ff6b56f6789084411d59
Susan Strickler <srs218@users.noreply.github.com>
2020-07-22 19:06:26 -0400

    Update rnaseq_cleanup.sh

--- annotation/rnaseq_cleanup.sh
+++ annotation/rnaseq_cleanup.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
 #Map RNA-seq data
-screen -L ./hisat_se_annot.sh . 10
+hisat2-build Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa Ugibba_FLYE_assembly.fasta.PolcaCorrected
+hisat2 --max-intronlen 100000 --dta -p $CPU -x Ugibba_FLYE_assembly.fasta.PolcaCorrected -U $file -S $samfile
+
+#run stringtie
+stringtie --rf -p 20 -o Ugibba_stemR1_st.gtf Ugibba_stemR1.sort.bam
+
+#For mapping many files:
+#screen -L ./hisat_se_annot.sh . 10
 
 #run Portcullis: https://bioconda.github.io/recipes/portcullis/README.html
-docker run --rm -v /home/srs57/Botany_workshop_2020/:/data maplesond/portcullis:stable portcullis full -t 20 -v /data/assemblies/Ugibba/PNAS2017/Utricularia_gibba.faa /data/Ugibba_transcriptome_data/Ugibba_bladderR1.fastq.bam -o Ugibba_bladderR1_portls home/srs57/Botany_workshop_2020/
+docker run --rm -v /home/srs57/Botany_workshop_2020/Ugibba_transcriptome_data:/data maplesond/portcullis:stable portcullis full -t 7 -v /data/Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa /data/Ugibba_stemR1.sort.bam -o Ugibba_stemR1_port
 
 #run Mikado: https://bioconda.github.io/recipes/mikado/README.htm		l
-mikado configure --list list.txt --reference ../../Asclepias_syriaca_v0.3_chromosomes_and_contigs.fasta --mode permissive --scoring plants.yaml  --copy-scoring plants.yaml --junctions all_junc_pass.bed  -bt /databases/plant_swissprot/uniprot_sprot_plants.fasta
+mikado configure --list list.txt --reference Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa --mode permissive --scoring plants.yaml  --copy-scoring plants.yaml --junctions Ugibba_stemR1_port/3-filt/portcullis_filtered.pass.junctions.bed  -bt /databases/plant_swissprot/uniprot_sprot_plants.fasta > configuration.yaml
+
 screen -L mikado prepare --json-conf configuration.yaml
 
 blastx -max_target_seqs 5 -num_threads 30 -query mikado_prepared.fasta -outfmt 5 -db /databases/plant_swissprot/uniprot_sprot_plants.fasta -evalue 0.000001 2> blast.log | sed '/^$/d' | gzip -c - > mikado.blast.xml.gz
