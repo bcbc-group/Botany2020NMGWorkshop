05591c498790df0ac6b538442b46765e0838a269
Susan Strickler <srs218@users.noreply.github.com>
2020-07-23 11:29:53 -0400

    Update maker.sh

--- annotation/maker.sh
+++ annotation/maker.sh
@@ -4,6 +4,12 @@ maker -CTL
 emacs maker_opts.ctl
 #genome=Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa.masked
 #protein=uniprot_sprot_plants.fasta
+#model_org= #all
 #repeat_protein= #/opt/maker/data/te_proteins.fasta 
 
- mpirun -np 6 maker
+screen -L mpirun -np 6 maker
+#put maker output on datastore
+
+gff3_merge -d 
+
+Busco
