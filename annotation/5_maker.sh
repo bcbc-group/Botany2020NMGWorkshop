b63f8a79d2d7bb1e2e7c87b97066c9d0068ed55b
Susan Strickler <srs218@users.noreply.github.com>
2020-07-28 07:23:23 -0400

    Update 5_maker.sh

--- annotation/5_maker.sh
+++ annotation/5_maker.sh
@@ -1,27 +1,32 @@
 #generate control files
 maker -CTL
 
+#edit the input control file
 emacs maker_opts.ctl
-#genome=Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa.masked
+#genome=contig_15_masked.fasta
 #protein=uniprot_sprot_plants.fasta
 #model_org= #all
 #repeat_protein= #/opt/maker/data/te_proteins.fasta 
+#snaphmm= #snap2.hmm
+#augustus_species=Ugibba
+#pred_gff=mikado.loci.gff3
+#est2genome=0
+#protein2genome=0
 
-screen -L mpirun -np 6 maker
+screen -L mpirun -np 7 maker -base maker1
 #put maker output on datastore
 
 #make a gff3 files from maker results
-gff3_merge -d Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa.all.gff
-gff3_merge -g -o Ugibba_FLYE_assembly.fasta.PolcaCorrected_maker_models.gff -d Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa.all.gff
+gff3_merge -d maker1.maker.output/maker1_master_datastore_index.log
+gff3_merge -g -o maker1_models.gff -d maker1.maker.output/maker1_master_datastore_index.log
 
 #maker a protein file from maker output
-gffread -g Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa -y Ugibba_maker_proteins.fasta Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa.all.gff
-gffread -g Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa -y Ugibba_maker_proteins.fasta Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa.all.gff
+gffread -g contig_15.fasta -y maker1_proteins.fasta maker1_models.gff
 
 #run BUSCO
 cd /scratch/annotation
 
 iget -rPT /iplant/home/shared/Botany2020NMGWorkshop/embryophyta_odb9
 
-docker run  -v /scratch/annotation:/busco_wd -w /busco_wd/  upendradevisetty/busco:v3.0 -i Ugibba_FLYE_assembly.fasta.PolcaCorrected.fa --out Ugibba_maker1 -c 6 --lineage_path embryophyta_odb9/ --mode prot
+docker run  -v /scratch/annotation:/busco_wd -w /busco_wd/  upendradevisetty/busco:v3.0 -i maker1_proteins.fasta --out Ugibba_maker1 -c 7 --lineage_path embryophyta_odb9/ --mode prot
 #/tools/busco/scripts/run_BUSCO.py -i Ugibba_maker_proteins.fasta -c 8 -m prot -l /tools/busco/embryophyta_odb10 -o Ugibba_maker1
